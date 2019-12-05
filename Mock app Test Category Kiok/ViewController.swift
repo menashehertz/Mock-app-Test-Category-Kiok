0499d1effd23a509b811ead5bddd664f689669ed
Steven Hertz <hertzsteven@icloud.com>
2019-12-05 08:33:44 -0500

    Initial Commit

---
+++ Mock app Test Category Kiok/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mock app Test Category Kiok
+//
+//  Created by Steven Hertz on 12/5/19.
+//  Copyright © 2019 DevelopItSolutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
